d0faf52c1f3c7dbc36efa783667d7bb349e92897
Julian Builes <jlnbuiles@gmail.com>
2023-04-24 20:26:43 -0500

    Initial Commit

---
+++ SimpsonsCharacterViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonsCharacterViewer
+//
+//  Created by Julian Builes on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
